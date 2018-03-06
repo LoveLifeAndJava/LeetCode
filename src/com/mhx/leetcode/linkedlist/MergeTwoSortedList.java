6579917c87c3a2334e2ba609d0099f53d640c858
mhx <mhx8888@gmail.com>
2018-03-07 00:27:11 +0800

    21.Merge Two Sorted Linked Lists

---
+++ src/com/mhx/leetcode/linkedlist/MergeTwoSortedList.java
@@ -0,0 +1,68 @@
+package com.mhx.leetcode.linkedlist;
+
+/**
+ * @author MHX
+ * @date 2018/3/6
+ */
+public class MergeTwoSortedList {
+
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null && l2 == null) {
+            return null;
+        }
+
+        if (l1 == null) {
+            return l2;
+        }
+
+        if (l2 == null) {
+            return l1;
+        }
+
+        // Use a fake head to avoid checking edges
+        ListNode newNode = new ListNode(0);
+        /*if (l1.val < l2.val) {
+            newNode = l1;
+            l1 = l1.next;
+        } else {
+            newNode = l2;
+            l2 = l2.next;
+        }*/
+
+        ListNode p = newNode;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                p.next = l1;
+                l1 = l1.next;
+            } else {
+                p.next = l2;
+                l2 = l2.next;
+            }
+            p = p.next;
+        }
+
+        if (l1 != null) {
+            p.next = l1;
+        }
+        if (l2 != null) {
+            p.next = l2;
+        }
+
+        return newNode.next;
+    }
+
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(1);
+        l1.next = new ListNode(2);
+        l1.next.next = new ListNode(4);
+        System.out.println(l1.printList());
+
+        ListNode l2 = new ListNode(1);
+        l2.next = new ListNode(3);
+        l2.next.next = new ListNode(4);
+        System.out.println(l2.printList());
+
+        MergeTwoSortedList mergeTwoSortedList = new MergeTwoSortedList();
+        System.out.println(mergeTwoSortedList.mergeTwoLists(l1, l2).printList());
+    }
+}
